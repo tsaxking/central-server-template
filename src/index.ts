e3c207caf71af325910479e203c9da7a4c06dac5
taylor <taylorking206@u.boisestate.edu>
2024-12-30 23:13:08 -0700

    init trpc

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello via Bun!");
+import { Account } from './structs/account';
+import { router, publicProcedure } from './utils/trpc';
+import { z } from 'zod';
+import { createHTTPServer } from '@trpc/server/adapters/standalone';
+
+
+const appRouter = router({
+    userList: publicProcedure
+        .input(z.string())
+        .query(async (opts) => {
+            const { input } = opts;
+            // return (await Account.Account.all(false)).unwrap().map(a => a.safe());
+            return (await Account.Account.fromId(input)).unwrap()?.safe();
+        }),
+});
+// Export type router type signature,
+// NOT the router itself.
+export type AppRouter = typeof appRouter;
+
+
+const server = createHTTPServer({
+    router: appRouter,
+});
+
+server.listen(3000);
